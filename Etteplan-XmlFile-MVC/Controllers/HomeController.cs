41a1fd51709bcda43ced1be63573e8ca6d6fa128
garyaldahoul <gary.gds@gmail.com>
2023-08-04 13:46:28 +0200

    read XML file simple MVC

---
+++ Etteplan-XmlFile-MVC/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Etteplan_XmlFile_MVC.Models;
+using System.Xml;
+
+namespace Etteplan_XmlFile_MVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        TransUnitModel transUnit = new TransUnitModel();
+        transUnit.TransUnits = new List<TransUnit>();
+
+        XmlDocument doc = new XmlDocument();
+        string xmlFilePath = "/Users/User/Projects/Etteplan-XmlFile-MVC/Etteplan-XmlFile-MVC/XMLFile/sma_gentext.xml";
+        doc.Load("/Users/User/Projects/Etteplan-xml-consoleDB/Etteplan-xml-consoleDB/XMLFile/sma_gentext.xml");
+        XmlNodeList xmlNodeList = doc.GetElementsByTagName("trans-unit");
+
+        for (int i = 0; i < xmlNodeList.Count; i++)
+        {
+            transUnit.TransUnits.Add(new TransUnit
+            {
+                Id = Int32.Parse(xmlNodeList[i].Attributes[0].Value),
+                target = xmlNodeList[i].SelectSingleNode("target").InnerText.ToString(),
+                source = xmlNodeList[i].SelectSingleNode("source").InnerText.ToString()
+            }) ;
+        }
+
+
+        return View(transUnit);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ Etteplan-XmlFile-MVC/Models/TransUnitModel.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+namespace Etteplan_XmlFile_MVC.Models
+{
+	public class TransUnitModel
+	{
+		public List<TransUnit> TransUnits { get; set; }
+	}
+    public class TransUnit
+    {
+        public int Id { get; set; }
+        public string target { get; set; }
+        public string source { get; set; }
+    }
+}
+
