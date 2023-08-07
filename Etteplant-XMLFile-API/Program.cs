2b14570d9228cfd0291c86d4e05bed87a19ac616
garyaldahoul <gary.gds@gmail.com>
2023-08-07 20:22:16 +0200

    Write data XML file API/DB

--- Etteplant-XMLFile-API/Program.cs
+++ Etteplant-XMLFile-API/Program.cs
@@ -13,36 +13,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(connectionString));
 
-
-static void InsertDataFromXMLFileToDB(string connectionString)
-{
-    SqlConnection connection = new SqlConnection(connectionString);
-    XmlDocument doc = new XmlDocument();
-    // I used route import pass because I using mac os machin for development
-    doc.Load("/Users/User/Projects/Etteplant-XMLFile-API/Etteplant-XMLFile-API/Data/XMLFile/sma_gentext.xml");
-    XmlNodeList xmlNodeList = doc.GetElementsByTagName("trans-unit");
-
-
-    connection.Open();
-    for (int i = 0; i < xmlNodeList.Count; i++)
-    {
-
-        string sql = "Insert INTO [TransUnits] (Id,Target,Source) values (@Id,@Target,@Source)";
-
-        SqlCommand sqlCommand = new SqlCommand(sql, connection);
-
-        sqlCommand.Parameters.AddWithValue("@Id", Int32.Parse(xmlNodeList[i].Attributes[0].Value));
-        sqlCommand.Parameters.AddWithValue("@Target", xmlNodeList[i].SelectSingleNode("target").InnerText.ToString());
-        sqlCommand.Parameters.AddWithValue("@Source", xmlNodeList[i].SelectSingleNode("source").InnerText.ToString());
-
-        sqlCommand.ExecuteNonQuery();
-
-    }
-
-    connection.Close();
-
-}
-
 var app = builder.Build();
 
 app.UseCors(options => options.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader());
@@ -60,7 +30,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-
-
-InsertDataFromXMLFileToDB(connectionString);
